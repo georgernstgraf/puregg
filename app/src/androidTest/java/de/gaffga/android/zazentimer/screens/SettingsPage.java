a4b8702d9ce05f2e5e2fc0d3647db9b99fac52e7
Georg Graf (SPG) <grafg@spengergasse.at>
2026-05-02 01:48:18 +0200

    fix: Use RecyclerViewActions.scrollToPosition for PreferenceFragmentCompat scrolling (#84)
    - scrollTo() with withText() does not work reliably for PreferenceFragmentCompat
      on CI because the preference list uses a RecyclerView internally
    - scrollToPosition(99) scrolls to the bottom of the preference list, making
      backup/restore preferences visible before clicking them
    - Fixes testRestore and testBackup failures on both API 29 and API 36 CI

--- app/src/androidTest/java/de/gaffga/android/zazentimer/screens/SettingsPage.java
+++ app/src/androidTest/java/de/gaffga/android/zazentimer/screens/SettingsPage.java
@@ -6,6 +6,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -14,10 +15,9 @@ import android.os.SystemClock;
 
 import de.gaffga.android.zazentimer.R;
 
-/**
- * Page object for the settings screen.
- */
 public class SettingsPage extends BasePage {
+
+    private static final int PREF_RECYCLER_ID = android.R.id.list;
 
     public SettingsPage() {
         try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
@@ -25,10 +25,11 @@ public class SettingsPage extends BasePage {
             onView(withText(R.string.pref_title_backup)).perform(scrollTo());
         } catch (Exception e) {
             try {
-                onView(withId(android.R.id.list)).perform(swipeUp());
+                onView(withId(PREF_RECYCLER_ID)).perform(scrollToPosition(99));
                 SystemClock.sleep(500);
             } catch (Exception ignored) {}
         }
+    }
     }
 
     /**
@@ -36,11 +37,9 @@ public class SettingsPage extends BasePage {
      */
     public SettingsPage clickBackup() {
         try {
-            onView(withText(R.string.pref_title_backup)).perform(scrollTo());
-        } catch (Exception ignored) {
-            try { onView(withId(android.R.id.list)).perform(swipeUp()); SystemClock.sleep(300); } catch (Exception ignored2) {}
-        }
-        SystemClock.sleep(300);
+            onView(withId(PREF_RECYCLER_ID)).perform(scrollToPosition(99));
+            SystemClock.sleep(300);
+        } catch (Exception ignored) {}
         onView(withText(R.string.pref_title_backup)).perform(click());
         return this;
     }
@@ -51,11 +50,9 @@ public class SettingsPage extends BasePage {
      */
     public SettingsPage clickRestoreAndConfirm() {
         try {
-            onView(withText(R.string.pref_title_restore)).perform(scrollTo());
-        } catch (Exception ignored) {
-            try { onView(withId(android.R.id.list)).perform(swipeUp()); SystemClock.sleep(300); } catch (Exception ignored2) {}
-        }
-        SystemClock.sleep(300);
+            onView(withId(PREF_RECYCLER_ID)).perform(scrollToPosition(99));
+            SystemClock.sleep(300);
+        } catch (Exception ignored) {}
         onView(withText(R.string.pref_title_restore)).perform(click());
         SystemClock.sleep(300);
         onView(withText(R.string.ok)).perform(click());
