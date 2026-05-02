e7d72249e8552c499081ae03563fb1360f301abe
Georg Graf (SPG) <grafg@spengergasse.at>
2026-05-02 02:27:10 +0200

    fix: Retry scrollTo for preference items with delay (#84)
    - scrollTo() with withText() should work for PreferenceFragmentCompat
      but may fail on slow CI emulators if the preference list hasn't loaded
    - Add retry loop (3 attempts with 500ms delay) for scrollTo before clicking
    - Remove complex RecyclerView matching approach

--- app/src/androidTest/java/de/gaffga/android/zazentimer/screens/SettingsPage.java
+++ app/src/androidTest/java/de/gaffga/android/zazentimer/screens/SettingsPage.java
@@ -9,61 +9,35 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.os.SystemClock;
-import android.view.View;
-import android.view.ViewParent;
-
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.test.espresso.PerformException;
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.espresso.util.HumanReadables;
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 
 import de.gaffga.android.zazentimer.R;
 
 public class SettingsPage extends BasePage {
 
     public SettingsPage() {
-        try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
-        try {
-            onView(withText(R.string.pref_title_backup)).perform(scrollTo());
-        } catch (Exception e) {
-            scrollPreferencesToBottom();
+        try { Thread.sleep(1500); } catch (InterruptedException ignored) {}
+    }
+
+    private void scrollToPreference(int textResId) {
+        for (int attempt = 0; attempt < 3; attempt++) {
+            try {
+                onView(withText(textResId)).perform(scrollTo());
+                onView(withText(textResId)).check(matches(isDisplayed()));
+                return;
+            } catch (Exception e) {
+                SystemClock.sleep(500);
+            }
         }
     }
 
-    private void scrollPreferencesToBottom() {
-        try {
-            onView(isRecyclerView()).perform(RecyclerViewActions.scrollToPosition(99));
-            SystemClock.sleep(500);
-        } catch (Exception ignored) {}
-    }
-
-    private static Matcher<View> isRecyclerView() {
-        return new TypeSafeMatcher<View>() {
-            @Override
-            protected boolean matchesSafely(View view) {
-                return view instanceof RecyclerView;
-            }
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("is a RecyclerView");
-            }
-        };
-    }
-
     public SettingsPage clickBackup() {
-        scrollPreferencesToBottom();
+        scrollToPreference(R.string.pref_title_backup);
         onView(withText(R.string.pref_title_backup)).perform(click());
         return this;
     }
 
     public SettingsPage clickRestoreAndConfirm() {
-        scrollPreferencesToBottom();
+        scrollToPreference(R.string.pref_title_restore);
         onView(withText(R.string.pref_title_restore)).perform(click());
         SystemClock.sleep(300);
         onView(withText(R.string.ok)).perform(click());
